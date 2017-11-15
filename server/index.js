bb8d4c6999238f0859cd9c518d2bcfc04cfec06a
EnkhtushigNamkhai <enkhtushig@gmail.com>
2017-11-15 10:44:41 -0800

    Setting up the package json and dependencies

---
+++ server/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 5000
+app.listen(port);
+console.log('app is listening on 5000');
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.end("Hello2");
+} )

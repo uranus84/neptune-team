3c9c81a93343df85308cc17cf498a9903a983bcf
Donovon <donovonjenson@hotmail.com>
2017-11-21 20:45:40 -0800

    render photo && walkability score

--- client/src/PhotoInfo.jsx
+++ client/src/PhotoInfo.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 
 var PhotoInfo = (props) => (
-<p className="photoblock_content">Photo goes Here</p>
+<p className="photoblock_content">
+	<img src={props.photoUrl} />
+</p>
 
   )
 

bc34e08140b986c992e25f57293a77814a6d8114
Michael Neveu <mbneveu@gmail.com>
2017-11-21 16:14:19 -0800

    skeleton and css complete with gitignore functional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,60 @@
 import React from 'react';
+import MappComponent from './MappComponent.jsx';
+import PhotoInfo from './PhotoInfo.jsx';
+import TipsBlock from './TipsBlock.jsx';
+import TopTweetsInfo from './TopTweetsInfo.jsx';
+import WalkabilityInfo from './WalkabilityInfo.jsx';
+import TwitterTrends from './TwitterTrends.jsx';
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
- 
+
   render() {
-    return (
-      <div>
-        <p>Test</p>
-        <p>Some Paragraph here</p>
-      </div>
+    return (    
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12-sm" id="titlebar">
+              <h1 id="titlefont">InfoMapp</h1>
+              <h2 id="subtitlefont">Learn More With Just A Click</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-6">  
+              <div id="mapblock" className="vertical-center">         
+                <MappComponent />
+              </div>
+              <div id="twittertrends">
+                <TwitterTrends />
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div id="infoblock">
+                <div id="walkabilityblock">
+                  <WalkabilityInfo />
+                </div>
+                <div id="toptweetsblock">
+                  <TopTweetsInfo />
+                </div>
+                <div id="photoblock">
+                  <PhotoInfo />
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div id ="tipsblock">
+                <TipsBlock />
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 }
+
 export default App;
+
+
---
+++ client/src/WalkabilityInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+var WalkabilityInfo = (props) => (
+<p className="walkabilityblock_content">Walkability Info Goes Here</p>
+
+  )
+
+export default WalkabilityInfo;

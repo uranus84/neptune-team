358cbb4ba60f63d9bf71d7bf6526a2b5242cc7a4
Donovon <donovonjenson@hotmail.com>
2017-11-24 13:03:22 -0800

    fix lat/lon in walkscore + add env options for api keys

--- server/index.js
+++ server/index.js
@@ -21,11 +21,11 @@ app.listen(port, function () {
 app.get('/googlepics', (req,res) => {
 	//Gets a location based on a long/latitude
 	var locationData = req.query.lat + ', ' + req.query.lon; 
-	axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {params : {key: key.GOOGLE_MAP_API, location: locationData, radius :2000}})
+	axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {params : {key: (process.env.MAP_API || key.GOOGLE_MAP_API), location: locationData, radius :2000}})
 	.then ((results) =>{
 		//Pulls the first image information
 		var photoString = results.data.results[0].photos[0].photo_reference;
-		var imageUrl = 'https://maps.googleapis.com/maps/api/place/photo?maxheight=290&key=' + key.GOOGLE_MAP_API + '&photoreference=' + photoString;
+		var imageUrl = 'https://maps.googleapis.com/maps/api/place/photo?maxheight=290&key=' + (process.env.MAP_API || key.GOOGLE_MAP_API) + '&photoreference=' + photoString;
 		//Sends back a direct Url to reach the image.
 		res.end(imageUrl)
 	})
@@ -38,7 +38,8 @@ app.get('/googlepics', (req,res) => {
 })
 
 app.get('/walkscore', (req,res) => {
-	axios.get('http://api.walkscore.com/score', {params: {lat: 37.773972, lon: -122.431297, wsapikey: key.WALKSCORE_API, address: '', format:JSON}})
+	var locationData = {lat: req.query.lat, lon :req.query.lon}
+	axios.get('http://api.walkscore.com/score', {params: {lat: locationData.lat, lon: locationData.lon, wsapikey: (process.env.WALKSCORE_API || key.WALKSCORE_API), address: '', format:JSON}})
 	.then ((results)=>{
 		console.log(results);
 		//Returned value is a weird string, splitting it to get just the walk score out

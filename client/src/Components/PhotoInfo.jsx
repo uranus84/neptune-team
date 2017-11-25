37244e8ff43c3deeff0b3556bd602d62e36d38b5
Ifi <spiffyock@gmail.com>
2017-11-25 02:38:02 -0500

    push through trends data. refactor rendering to add loading circles. refactor getCityState to get the short name of the city for twitter searches

--- client/src/Components/PhotoInfo.jsx
+++ client/src/Components/PhotoInfo.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-var PhotoInfo = (props) => (
-  <p className="photoblock_content">
-    <img src={props.photoUrl} />
-  </p>
-)
+var PhotoInfo = (props) => {
+  if(props.photoUrl === "") {
+    return (<div className="loader"></div>)
+  } else {
+    return (
+      <p className="photoblock_content">
+        <img src={props.photoUrl} />
+      </p>
+    )
+  }
+}
 
 export default PhotoInfo;
--- client/src/Components/TwitterTrends.jsx
+++ client/src/Components/TwitterTrends.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-var TwitterTrends = (props) => (
-  <div>
-    <p className="twittertrends_content">Twitter Trends Go Here!</p>
-  </div>
-
-  )
+var TwitterTrends = (props) => {
+  if(props.recentTweetsFrom === "" || props.recentTweetsAbout === "" || props.oldTweetsFrom === "" || props.oldTweetsAbout === "") {
+    return (<div className="loader"></div>)
+  } else {
+    return (
+      <div>
+        <p className="twittertrends_content">Twitter Trends Go Here!</p>
+        <div>{JSON.stringify(props.recentTweetsFrom)}</div>
+        <div>{JSON.stringify(props.recentTweetsAbout)}</div>
+        <div>{JSON.stringify(props.oldTweetsFrom)}</div>
+        <div>{JSON.stringify(props.oldTweetsAbout)}</div>
+      </div>
+    )
+  }
+}
 
 
 export default TwitterTrends;

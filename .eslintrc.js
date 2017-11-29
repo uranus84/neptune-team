9eaf9a192442d47a0760821ba6c556a98518553f
aploftus <aploftus@users.noreply.github.com>
2017-11-28 16:28:39 -0800

    (doc) Add linter functionality, run fix flag on inherited codebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,30 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
+// These rules enforce the Hack Reactor Style Guide
+// Visit this repo for more information:
+// https://github.com/reactorcore/eslint-config-hackreactor
+ 
 
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  extends: [
+    "./node_modules/eslint-config-hackreactor/index.js",
+    "plugin:react/recommended"
+  ],
+  "plugins": [
+    "react"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true,
+    "mocha": true
+  },
+  "rules": {
+    "camelcase": "off"
+  }
 };

e989f8cb82ae2af6650a6ae31de75e15fc057c66
aploftus <aploftus@users.noreply.github.com>
2017-11-30 15:43:41 -0800

    (bug) Update file path for the components being tested

--- tests/adminPanel.test.js
+++ tests/adminPanel.test.js
@@ -4,9 +4,8 @@ import renderer from 'react-test-renderer';
 import { shallow, mount, render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import AdminPanel from '../client/src/AdminPanel.jsx';
-import AdminTipList from '../client/src/AdminTipList.jsx';
-import AdminTipEntry from '../client/src/AdminTipEntry.jsx';
+import AdminPanel from '../client/src/loggedin/AdminPanel.jsx';
+import AdminTipEntry from '../client/src/loggedin/AdminTipEntry.jsx';
 
 configure({ adapter: new Adapter() });
 
@@ -15,20 +14,6 @@ describe('<AdminPanel />', () => {
     const component = shallow(<AdminPanel />);
     expect(component).toHaveLength(1);
   });
-
-  // it('renders AdminTipList component', () => {
-  //   const tips = [
-  //     {
-  //       city: 'San Francisco',
-  //       state: 'CA',
-  //       name: 'amy',
-  //       tiptext: 'hello world',
-  //       datecreated: '2017-11-18 09:09:09'
-  //     }
-  //   ];
-  //   const component = shallow(<AdminTipList tips={tips} />);
-  //   expect(component).toHaveLength(1);
-  // });
 
   it('renders AdminTipEntry component', () => {
     const tip = {

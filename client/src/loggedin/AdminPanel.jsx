8777493c1a2b67869e0d0d74bce0768fd547b262
aploftus <aploftus@users.noreply.github.com>
2017-11-30 10:46:44 -0800

    (feat) Render admin panel skeleton as table with dummy data

--- client/src/loggedin/AdminPanel.jsx
+++ client/src/loggedin/AdminPanel.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import AdminTipList from './AdminTipList.jsx';
 
-const AdminPanel = (props) => (
-  <div>
-    <p>hidden content123</p>
-  </div>
-);
+class AdminPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tips: [
+        {
+          ID: 1,
+          city: 'san francisco',
+          datecreated: '2017-11-30T03:41:47.000Z',
+          name: 'James Dean',
+          state: 'california',
+          tiptext: 'Be sure to see the Golden Gate park'
+        }
+      ]
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>Moderate Tips</h1>
+        <h3>All Submitted Tips</h3>
+        <AdminTipList tips={this.state.tips} />
+      </div>
+    );
+  }
+}
 
 export default AdminPanel;

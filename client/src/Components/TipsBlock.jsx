5f599793d11b7fa1c292f557dd9934002a8a422f
aploftus <aploftus@users.noreply.github.com>
2017-11-30 11:34:56 -0800

    (chore) Cleanup spacing issues for linter, comment out admin fn

--- client/src/Components/TipsBlock.jsx
+++ client/src/Components/TipsBlock.jsx
@@ -14,13 +14,23 @@ class TipsBlock extends React.Component {
       tipsState: '',
       tipsName: '',
       tipsContent: '',
-      currentlyDisplayedTips: [{city: this.props.info.city, name: 'None', state: this.props.info.state, tiptext: 'None'}]
+      currentlyDisplayedTips: [
+        {
+          city: this.props.info.city,
+          name: 'None',
+          state: this.props.info.state,
+          tiptext: 'None'
+        }
+      ]
     };
   }
 
   componentWillReceiveProps () {
     this.getTips();
-    this.setState({tipsCity: this.capitalizeFirstLetterOnly(this.props.info.city), tipsState: this.capitalizeFirstLetterOnly(this.props.info.state)});
+    this.setState({
+      tipsCity: this.capitalizeFirstLetterOnly(this.props.info.city),
+      tipsState: this.capitalizeFirstLetterOnly(this.props.info.state)
+    });
   }
 
   componentWillMount () {
@@ -34,7 +44,7 @@ class TipsBlock extends React.Component {
 
   submitTips () {
     var contexthere = this;
-    var stateList =  {     
+    var stateList = {     
       arizona: 'az',
       alabama: 'al',
       alaska: 'ak',
@@ -90,7 +100,7 @@ class TipsBlock extends React.Component {
     var stateValidatorAndFormatter = function (string) {
       var stringLowerCase = string.toLowerCase();
       var isState = false;
-      if (stateList.hasOwnProperty(stringLowerCase)) {isState = true}
+      if (stateList.hasOwnProperty(stringLowerCase)) { isState = true; }
       if (isState === false) {
         for (var k in stateList) {
           if (stateList[k] === stringLowerCase) {
@@ -100,14 +110,19 @@ class TipsBlock extends React.Component {
         }  
       }
       if (isState === false) { return false; }
-      if (isState === true) {contexthere.setState({tipsState: stringLowerCase})}
+      if (isState === true) { contexthere.setState({ tipsState: stringLowerCase }); }
       return stringLowerCase;   
     };
     
-    if(!stateValidatorAndFormatter(this.state.tipsState)) {
+    if (!stateValidatorAndFormatter(this.state.tipsState)) {
       alert(this.state.tipsState + ' Is Not A State, Please Try Again');
     } else {
-      axios.post('/tips', {cityData: this.state.tipsCity, stateData: stateValidatorAndFormatter(this.state.tipsState), nameData: this.state.tipsName, tipData: this.state.tipsContent })
+      axios.post('/tips', {
+        cityData: this.state.tipsCity,
+        stateData: stateValidatorAndFormatter(this.state.tipsState),
+        nameData: this.state.tipsName,
+        tipData: this.state.tipsContent
+      })
         .then(function (response) {
           // console.log('/tips POST WORKED RESPONSE = ', response);
           contexthere.setState({tipsCity: '', tipsState: '', tipsName: '', tipsContent: ''});
@@ -115,19 +130,28 @@ class TipsBlock extends React.Component {
           // console.log('FINAL RAN!!!');
         })
         .catch(function (error) {
-          console.log('/tips POST ERROR',error);
+          console.log('/tips POST ERROR', error);
         });
     }
   }
 
   getTips () {
     var context = this;
-    axios.get('/tips', {params: {city: this.props.info.city.toLowerCase(), state: this.props.info.state.toLowerCase()}})
+    axios.get('/tips', { params: {
+      city: this.props.info.city.toLowerCase(),
+      state: this.props.info.state.toLowerCase()
+    }})
       .then(function (response) {
         // console.log('/tips GET WORKED RESPONSE = ', response.data);
         // console.log('CHECK THIS OUT!!!! = ', response.data.length);
         if (response.data.length < 1) {
-          var notes = {city: context.props.info.city, datecreated: "", name: "No data", state: context.props.info.state, tiptext: "No Tips Provided Yet"}
+          var notes = {
+            city: context.props.info.city,
+            datecreated: '',
+            name: 'No data',
+            state: context.props.info.state,
+            tiptext: 'No Tips Provided Yet'
+          };
           response.data = [notes];
         }
         context.setState({currentlyDisplayedTips: response.data});
@@ -144,83 +168,145 @@ class TipsBlock extends React.Component {
   capitalizeFirstLetterOnly (str) {
     var strArray = str.split(' ');
     for (var i = 0; i < strArray.length; i++) {
-      strArray[i] = strArray[i].substring(1, 0).toUpperCase()+strArray[i].substring(1).toLowerCase();
+      strArray[i] = `${strArray[i].substring(1, 0).toUpperCase()}${strArray[i].substring(1).toLowerCase()}`;
     }
     strArray = strArray.join(' ');
     return strArray;
-  };
+  }
 
   expandTipsDisplayFn () {
-    this.setState({areTipsExpanded: !this.state.areTipsExpanded});
+    this.setState({ areTipsExpanded: !this.state.areTipsExpanded });
   }
 
   //TEST ADMIN LOGIN
-  adminAccessFn () {
-    axios.get('/admin')
-      .then(function (response) {
-        console.log('/admin GET WORKED RESPONSE = ', response.data);
-      })
-      .catch(function (error) {
-        console.log('/admin GET ERROR', error);
-      });
-  }
+  // adminAccessFn () {
+  //   axios.get('/admin')
+  //     .then(function (response) {
+  //       console.log('/admin GET WORKED RESPONSE = ', response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log('/admin GET ERROR', error);
+  //     });
+  // }
 
   render () {
     // var stateStyleColor = {color: 'black', fontWeight: 'normal'};
-    if(this.state.userIntentCurrent === 'readTips') {
+    if (this.state.userIntentCurrent === 'readTips') {
       return (
         <div>
           <div>
             {/*<button onClick={this.getTips.bind(this)}>GET DATA TEST BUTTON</button>*/}
             {/*<button onClick={this.adminAccessFn.bind(this)}>TEST ON ADMIN ACCESS</button>*/}
             <h3>Tips From {this.capitalizeFirstLetterOnly(this.state.currentlyDisplayedTips[0].city)}</h3>
-            <button id="areTipsExpanded" className="btn btn-primary" style={{"margin": '2px'}} onClick={this.expandTipsDisplayFn.bind(this)}>Expand Tips For Easier Reading</button>
-            <button id="addTips" className="btn btn-primary" onClick={this.changeIntentFn.bind(this)}>I Want To ADD A Tip</button>
+            <button
+              id="areTipsExpanded"
+              className="btn btn-primary"
+              style={{ margin: '2px' }}
+              onClick={this.expandTipsDisplayFn.bind(this)}
+            >
+              Expand Tips For Easier Reading
+            </button>
+            <button 
+              id="addTips"
+              className="btn btn-primary"
+              onClick={this.changeIntentFn.bind(this)}
+            >
+              I Want To ADD A Tip
+            </button>
             <TipsList info={this.state.currentlyDisplayedTips}/>
           </div>
-          {this.state.areTipsExpanded ? <TipsPopUp info={this.state.currentlyDisplayedTips} clickCloseFn={this.expandTipsDisplayFn.bind(this)}/> : <div></div>}
+          {
+            this.state.areTipsExpanded ? <TipsPopUp
+              info={this.state.currentlyDisplayedTips} 
+              clickCloseFn={this.expandTipsDisplayFn.bind(this)}
+            /> : <div></div>
+          }
      
         </div>
-      )
+      );
     }
     if (this.state.userIntentCurrent === 'addTips') {
       return (
-      <div className="popup">
-        <div className="popup_inner">
-          
-          <button id="readTips" className="btn btn-primary" onClick={this.changeIntentFn.bind(this)}>I Want To READ Tips</button>
-          <button id="readTips" className="btn btn-primary toprightclass" onClick={this.changeIntentFn.bind(this)}>X</button>
-          
-          <h4>Add A Tip For <span style={{fontWeight: 'bold'}}>{this.capitalizeFirstLetterOnly(this.props.info.city)}</span> Below</h4>
-          <div>
+        <div className="popup">
+          <div className="popup_inner">
+            <button
+              id="readTips"
+              className="btn btn-primary"
+              onClick={this.changeIntentFn.bind(this)}
+            >
+              I Want To READ Tips
+            </button>
+            <button
+              id="readTips"
+              className="btn btn-primary toprightclass"
+              onClick={this.changeIntentFn.bind(this)}
+            >
+              X
+            </button>
+            
+            <h4>Add A Tip For 
+              <span style={{fontWeight: 'bold'}}>
+                {this.capitalizeFirstLetterOnly(this.props.info.city)}
+              </span>
+            Below</h4>
             <div>
-              <div className="popup_label">Name :</div>
-              <input type="text" id="tipsName" value={this.state.tipsName} onChange={this.changeHandlerFn.bind(this)}></input>
+              <div>
+                <div className="popup_label">Name :</div>
+                <input
+                  type="text"
+                  id="tipsName"
+                  value={this.state.tipsName}
+                  onChange={this.changeHandlerFn.bind(this)}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <div className="popup_label">City:</div>
-            {/*INPUT CITY*/}
-            <input type="text" id="tipsCity" value={this.state.tipsCity} onChange={this.changeHandlerFn.bind(this)}></input>
-          </div>
+            <div>
+              <div className="popup_label">City:</div>
+              {/*INPUT CITY*/}
+              <input
+                type="text"
+                id="tipsCity"
+                value={this.state.tipsCity}
+                onChange={this.changeHandlerFn.bind(this)}
+              />
+            </div>
 
-          <div>
-            <div className="popup_label">State:</div>
-            {/*INPUT CITY*/}
-            <input type="text" id="tipsState" value={this.state.tipsState} onChange={this.changeHandlerFn.bind(this)}></input>
-          </div>
+            <div>
+              <div className="popup_label">State:</div>
+              {/*INPUT CITY*/}
+              <input
+                type="text"
+                id="tipsState"
+                value={this.state.tipsState}
+                onChange={this.changeHandlerFn.bind(this)}
+              />
+            </div>
 
-          <div id='tipsContentWrapper'>
-            <div className="popup_label_tips_exception">Tip Goes Here:</div>
-            <textarea type="text" rows="5" id='tipsContent' style={{width: '50%', display: 'block'}} value={this.state.tipsContent} onChange={this.changeHandlerFn.bind(this)}></textarea>
-          </div>
+            <div id='tipsContentWrapper'>
+              <div className="popup_label_tips_exception">Tip Goes Here:</div>
+              <textarea
+                type="text"
+                rows="5"
+                id='tipsContent'
+                style={{ width: '50%', display: 'block' }}
+                value={this.state.tipsContent}
+                onChange={this.changeHandlerFn.bind(this)}
+              >
+              </textarea>
+            </div>
 
-          <button id="submitTips" className="btn btn-primary, redbtn" onClick={this.submitTips.bind(this)}>SUBMIT TIP</button>
-        
+            <button
+              id="submitTips"
+              className="btn btn-primary, redbtn"
+              onClick={this.submitTips.bind(this)}
+            >
+              SUBMIT TIP
+            </button>
+          
+          </div>
         </div>
-      </div>
-      )
+      );
     }
   }
 }

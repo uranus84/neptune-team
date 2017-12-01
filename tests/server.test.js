df8bd6093ac74f810a774fcadabe9948ef072321
aploftus <aploftus@users.noreply.github.com>
2017-11-30 19:19:28 -0800

    (test) Add tests for server route GET /tips

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 describe('server', () => {
   describe('GET requests to /admin', () => {
-    it('should respond to GET requests for /admin with a 200 status code', (done) => {
+    it('should respond with a 200 status code', (done) => {
       request('http://localhost:5000/admin', (error, response, body) => {
         expect(response.statusCode).to.equal(200);
         done();
@@ -31,13 +31,12 @@ describe('server', () => {
 
   describe('DELETE requests to /admin', () => {
     it('should delete a tip from the database', (done) => {
-      console.log('inside delete');
       // insert a tip to db, return its id
       request({
         method: 'POST',
         uri: 'http://localhost:5000/tips',
         json: {
-          cityData: 'berkeley',
+          cityData: 'san francisco',
           stateData: 'california',
           nameData: 'amy',
           tipData: 'test tip information'
@@ -60,6 +59,43 @@ describe('server', () => {
           });
         }
       });
+    });
+  });
+
+  describe('GET requests to /tips', () => {
+    it('should respond with a 200 status code', (done) => {
+      request('http://localhost:5000/tips', (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should send back parsable stringified JSON', (done) => {
+      request('http://localhost:5000/tips', (error, response, body) => {
+        expect(JSON.parse.bind(this, body)).to.not.throw();
+        done();
+      });
+    });
+
+    it('should return only approved tips', (done) => {
+      // should insert 2 tips to db, one approved and one rejected, then delete them after
+      // but for now, this will test existing tips
+      axios.get('http://localhost:5000/tips', { params: {
+        city: 'san francisco',
+        state: 'california'
+      }})
+        .then((response) => {
+          expect(response.data).to.be.an('array');
+          expect(response.data[0]).to.be.an('object');
+
+          let rejectedTipsCount = 0;
+          for (let i = 0; i < response.data.length; i++) {
+            if (response.data[i].status === 'rejected') { rejectedTipsCount++; }
+          }
+          expect(rejectedTipsCount).to.equal(0);
+          done();
+        })
+        .catch(err => console.log(err));
     });
   });
 });

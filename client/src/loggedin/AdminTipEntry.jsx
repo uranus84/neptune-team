39fd86346561eae5d7b2d580bc01658ffea51dd8
aploftus <aploftus@users.noreply.github.com>
2017-12-01 11:33:11 -0800

    (feat) Render rejected tips in different color

--- client/src/loggedin/AdminTipEntry.jsx
+++ client/src/loggedin/AdminTipEntry.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import moment from 'moment';
 
 var AdminTipEntry = (props) => {
+  let status = props.tip.status;
   if (props.tip.tiptext.length > 1) {
     return (
-      <tr>
+      <tr className={status}>
         <td>{moment(props.tip.datecreated).calendar()}</td>
         <td>{props.tip.name}</td>
         <td>{props.tip.tiptext}</td>
         <td>{props.tip.city}</td>
         <td>{props.tip.state}</td>
-        <td>{props.tip.status}</td>
+        <td>{status}</td>
         <td>
           <button className="btn btn-primary" onClick={(e) => props.deleteTip(props.tip.ID)}>
             Delete

f620c5c343617a39fa1a03b057bf910366de30fe
Kaiser Wu <kaiserwu@Kaisers-MacBook-Pro.local>
2017-12-02 15:43:43 -0800

    Add css styling and eggplant

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -6,7 +6,7 @@ class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'social'
+      view: 'home'
     };
     this.changeView = this.changeView.bind(this);
     this.renderView = this.renderView.bind(this);
@@ -25,19 +25,23 @@ class Index extends React.Component {
   render() {
     return (
       <div className="main">
-        <nav className="navbar">
+        <nav className="navbar navbar-light">
           <div className="col-sm-2" id="titlebar">
             <h1 id="titlefont">infoMapp</h1>
             <h2 id="subtitlefont">Learn More With Just A Click</h2>
           </div>
           <div className="col">
-            <button type="button" onClick={() => { this.changeView('home'); }}>Home</button>
-            <button type="button" onClick={() => { this.changeView('map'); }}>Map</button>
-            <button type="button" onClick={() => { this.changeView('social'); }}>Social</button>
-            <button type="button" onClick={() => { this.changeView('compare'); }}>Compare</button>
+            <button type="button" className="nav-item btn btn-primary btn-outline-success my-2 my-sm-0 nav-btn" onClick={() => { this.changeView('home'); }}>Home</button>
+            <button type="button" className="nav-item btn btn-primary btn-outline-success my-2 my-sm-0 nav-btn" onClick={() => { this.changeView('map'); }}>Map</button>
+            <button type="button" className="nav-item btn btn-primary btn-outline-success my-2 my-sm-0 nav-btn" onClick={() => { this.changeView('social'); }}>Social</button>
+            <button type="button" className="nav-item btn btn-primary btn-outline-success my-2 my-sm-0 nav-btn" onClick={() => { this.changeView('compare'); }}>Compare</button>
+            <button type="button" className="nav-item btn btn-primary btn-outline-success my-2 my-sm-0 nav-btn admin-btn pull-right" onClick={() => { this.changeView('admin'); }}>Admin</button>
           </div>
         </nav>
         <div className="row">
+
+        </div>
+        <div className="row content">
           {this.state.view === 'compare' ? (
             <div>
               {this.renderView()}

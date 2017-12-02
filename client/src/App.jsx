fb05b19cbfb120b88ca9ff79557793a81c988e93
Kaiser Wu <kaiser.a.wu@gmail.com>
2017-12-01 19:30:20 -0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -341,13 +341,3 @@ class App extends React.Component {
 }
 
 export default App;
-/*
-*/
-/*
-              <div id="infoblock">
-                <div className="row">
-
-                </div>
-                
-              </div>
-*/

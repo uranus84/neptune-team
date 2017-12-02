b1c137df5ccfd965e3b6d4dd10b0a9ea26c7c0ea
andy <chaochaoqi@gmail.com>
2017-12-01 20:57:50 -0800

    (feat) Login - server routes now present an object

--- database/passport.js
+++ database/passport.js
@@ -11,7 +11,7 @@ module.exports = function(passport) {
     scope           : ['user_friends'],
     profileFields   : ['id', 'email', 'first_name', 'last_name', 'friends']
   },
-  function(accessToken, refreshToken, profile, done) {
+  function(accessToken, refreshToken, profile, next) {
     console.log(profile);
     const user = {
       'firstName' : profile.name.givenName,
@@ -19,25 +19,25 @@ module.exports = function(passport) {
       'id'        : profile.id
     };
 
-    //where does this done function go?
+    //where does this next function go?
     Admin.isAdmin(user.id, function(err, rows) {
       console.log(`Userid is ${user.id}`);
       if (rows[0] === undefined) {
-        return done(null, false);
+        return next(null, false);
       } else if (err) {
-        return done(err, null);
+        return next(err, null);
       } else {
-        return done(null, profile);
+        return next(null, profile);
       }
     });
   }));
 
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser(function(user, next) {
     // console.log('wow dis is a user!', user);
-    done(null, user);
+    next(null, user);
   });
 
-  passport.deserializeUser(function(id, done) {
-    done(null, id);
+  passport.deserializeUser(function(id, next) {
+    next(null, id);
   });
 };

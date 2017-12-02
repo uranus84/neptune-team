e0683f3524cf65d676d7e64e267c907ae2b9cf55
aploftus <aploftus@users.noreply.github.com>
2017-12-01 16:10:49 -0800

    (feat) Render flag button for user

--- client/src/Components/TipsSpecificItems.jsx
+++ client/src/Components/TipsSpecificItems.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import moment from 'moment';
-//var moment = require('moment');
+import flag from '../img/flag.png';
 
-  var TipsSpecificItems = (props) => {
-    if (props.info.tiptext.length >1) {
-      return (
-        <div >
-          <li>
-            <p className='tipsSpecificItems'>{props.info.tiptext} <br/> -{props.info.name} <br/>{moment(props.info.datecreated).calendar()}</p>
-          </li>
-        </div>
-      )
-    }
+var TipsSpecificItems = (props) => {
+  if (props.info.tiptext.length > 1) {
+    return (
+      <div >
+        <li>
+          <p className='tipsSpecificItems'>{props.info.tiptext} <br/> -{props.info.name} <br/>{moment(props.info.datecreated).calendar()}</p>
+          <input type="image" className="btn-img" src={flag} />
+          <span className="flag-tip">Flag tip for removal</span>
+        </li>
+      </div>
+    );
   }
+};
   
  
 export default TipsSpecificItems;

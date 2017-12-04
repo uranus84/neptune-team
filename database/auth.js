8f6c55014b8d55d748d801b19d528a23e9e44e88
andy <chaochaoqi@gmail.com>
2017-12-03 16:50:37 -0800

    (feat) rebasing

--- database/auth.js
+++ database/auth.js
@@ -4,10 +4,10 @@ module.exports = {
     'clientSecret': '4861c1e79290c5724dd19f0f7db13fd9',
 
     //Uncomment for localhost callback
-    // 'callbackURL': 'http://localhost:5000/auth/facebook/callback'
+    'callbackURL': 'http://localhost:5000/auth/facebook/callback'
 
     //Uncomment for staging callback
-    'callbackURL': 'https://infomapp-staging.herokuapp.com/auth/facebook/callback'
+    // 'callbackURL': 'https://infomapp-staging.herokuapp.com/auth/facebook/callback'
 
     //Uncomment for production callback
     // 'callbackURL': 'https://infomapp.herokuapp.com/auth/facebook/callback'

1c305158db35bc8498019a1b7e4d9e272ea23bac
andy <chaochaoqi@gmail.com>
2017-12-08 20:42:40 -0800

    Updating env var

--- database/passport.js
+++ database/passport.js
@@ -1,13 +1,12 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var Admin = require('./index.js');
-var config = require('./auth.js');
 
 module.exports = function(passport) {
   passport.use(new FacebookStrategy({
-    clientID        : config.facebookAuth.clientID,
-    clientSecret    : config.facebookAuth.clientSecret,
-    callbackURL     : config.facebookAuth.callbackURL,
+    clientID        : process.env.clientID,
+    clientSecret    : process.env.clientSecret,
+    callbackURL     : process.env.callbackURL,
     scope           : ['user_friends', 'user_location'],
     profileFields   : ['id', 'email', 'first_name', 'last_name', 'friends', 'location']
   },
